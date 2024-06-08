a1fee54d07160f895a8e530552e85986c9f77d4d
Abhay Desai <abhaydesai110@gmail.com>
2024-06-09 00:51:11 +0530

    dashboard look

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,100 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { CircleChevronLeft } from "lucide-react";
+import Image from "next/image";
+import Logo from "../../public/Images/audi.svg";
+import "../assets/style.css";
 
 const page = () => {
+  const [menu, setMenu] = useState<Boolean>(false);
   return (
-    <main className='h-screen w-screen'>
-      <div className='w-full flex items-center justify-center'>
-        <div className=' w-[277px] min-h-screen bg-white border border border-black  text-addey-primary'>
-        asd
+    <main className="h-screen w-screen">
+      <div className="w-full flex relative">
+        <div
+          className={`${
+            menu ? " w-[277px]" : "w-[68px]"
+          }   h-screen bg-white border border-r border-addey-gray text-addey-primary shadow-xl  absolute anim px-3`}
+        >
+          <CircleChevronLeft
+            className="absolute right-0 top-3 translate-x-1/2"
+            onClick={() => {
+              setMenu(!menu);
+            }}
+          />
+          <div className="h-1/12 p-2 border-addey-gray flex items-center justify-center  border-b bg-white">
+            <Image className=" h-16" src={Logo} alt="" />
+          </div>
+          <div className="h-7/12  py-2">
+            <ul className="text-xl space-y-2">
+              <li
+                className={`w-full px-4 py-2 rounded flex items-center ${
+                  menu ? "" : "justify-center"
+                }   space-x-3 text-white bg-addey-primary`}
+              >
+                <span className="flex icon-home "></span>
+                <h6 className={`${menu ? "" : "hidden"}`}>Home</h6>
+              </li>
+              <li
+                className={`w-full px-4 py-2 rounded flex items-center ${
+                  menu ? "" : "justify-center"
+                }   space-x-3 hover:bg-addey-primary/10 text-[#8A92A6] anim`}
+              >
+                <span className="flex icon-earth  "></span>
+                <h6 className={`${menu ? "" : "hidden"}`}>Earth</h6>
+              </li>
+              <li
+                className={`w-full px-4 py-2 rounded flex items-center ${
+                  menu ? "" : "justify-center"
+                }   space-x-3 hover:bg-addey-primary/10 text-[#8A92A6] anim`}
+              >
+                <span className="flex icon-apple"></span>
+                <h6 className={`${menu ? "" : "hidden"}`}>Apple</h6>
+              </li>
+              <li
+                className={`w-full px-4 py-2 rounded flex items-center ${
+                  menu ? "" : "justify-center"
+                }   space-x-3 hover:bg -addey-primary/10 text-[#8A92A6] anim`}
+              >
+                <span className="flex icon-heart  "></span>
+                <h6 className={`${menu ? "" : "hidden"}`}>Heart</h6>
+              </li>
+              <li
+                className={`w-full px-4 py-2 rounded flex items-center ${
+                  menu ? "" : "justify-center"
+                }   space-x-3 hover:bg-addey-primary/10 text-[#8A92A6] anim`}
+              >
+                <span className="flex icon-gift  "></span>
+                <h6 className={`${menu ? "" : "hidden"}`}>Gift</h6>
+              </li>
+              <li
+                className={`w-full px-4 py-2 rounded flex items-center ${
+                  menu ? "" : "justify-center"
+                }   space-x-3 hover:bg-addey-primary/10 text-[#8A92A6] anim`}
+              >
+                <span className="flex icon-flag  "></span>
+                <h6 className={`${menu ? "" : "hidden"}`}>Fleg</h6>
+              </li>
+            </ul>
+          </div>
+          <div className="h-4/12 bg-lime-400"></div>
         </div>
-        <div className='w-[calc(100%-277px)] min-h-screen bg-addey-grayLight text-addey-primary'>
-          <div className='w-full '>
-            <div className='h-14 bg-red-200'></div>
+        <div
+          className={` ${
+            menu
+              ? "w-[calc(100%-277px)] ml-[277px]"
+              : " w-[calc(100%-68px)] ml-[68px]"
+          }     min-h-screen bg-addey-grayLight text-addey-primary anim`}
+        >
+          <div className="w-full h-screen">
+            <div className="h-14 bg-white border-b border-addey-gray shadow-md"></div>
+            <div className="h-[calc(100%-56px)]  overflow-auto">
+              <div className="h-[1000px] overflow-auto"></div>
+            </div>
           </div>
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,14 +51,14 @@ const config: Config = {
       colors: {
         addey: {
           primary: {
-            DEFAULT: "#0B2D61",
+            DEFAULT: "#3A57E8",
           },
           secondary: {
             DEFAULT: "#EE3139",
           },
           dark: "#01183A",
           darkPlus: "#021C44",
-          gray: "#444444",
+          gray: "#E9EBF0",
           grayLight: "#f4f5f7",
           ceil: "#8ba1c4",
           jpIndigo: "#283a56",
@@ -76,8 +76,23 @@ const config: Config = {
       animation: {
         zoom: "zoom 1s ease-in-out infinite",
       },
+      height: {
+        "1/12": "8.333333%",
+        "2/12": "16.666667%",
+        "3/12": "25%",
+        "4/12": "33.333333%",
+        "5/12": "41.666667%",
+        "6/12": "50%",
+        "7/12": "58.333333%",
+        "8/12": "66.666667%",
+        "9/12": "75%",
+        "10/12": "83.333333%",
+        "11/12": "91.666667%",
+        "12/12": "100%",
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
