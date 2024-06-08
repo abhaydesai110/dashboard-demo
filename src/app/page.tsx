9f56aa3a0b2634f67b95d39d878bd88c8ce8da56
Abhay Desai <abhaydesai110@gmail.com>
2024-06-08 15:55:52 +0530

    dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main className=''>
-      setup done...starting.... 
+    <main className='h-screen w-screen'>
+      <div className='w-full flex items-center justify-center'>
+        <div className=' w-[277px] min-h-screen bg-white border border border-black  text-addey-primary'>
+        asd
+        </div>
+        <div className='w-[calc(100%-277px)] min-h-screen bg-addey-grayLight text-addey-primary'>
+          <div className='w-full '>
+            <div className='h-14 bg-red-200'></div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
